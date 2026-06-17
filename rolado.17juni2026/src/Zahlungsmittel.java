b3a38bc2b4079395427baddf9c7ea523dd985e93
deaddls <andrew.simon@gmx.at>
2026-06-17 16:16:21 +0200

    Wundabaaaa (Bar)

--- rolado.17juni2026/src/Zahlungsmittel.java
+++ rolado.17juni2026/src/Zahlungsmittel.java
@@ -1,7 +1,6 @@
 public class Zahlungsmittel {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Bargeld Challenge");
-        System.out.println("Wunderbaren Scheine");
+        System.out.println("Baageld");
     }
 }
