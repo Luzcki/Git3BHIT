0f28a565bf1e3463a2c083eb619dd8fb0a1b1bb3
Luzki <orlando.gross.2009@gmail.com>
2026-06-17 16:20:49 +0200

    Update Zahlungsmittel.java

--- Zahlungsmittel.java
+++ Zahlungsmittel.java
@@ -1,7 +1,6 @@
 public class Zahlungsmittel {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-                System.out.println("PRINT BARGELD, World!");
-
+        System.out.println("PRINT BARGELD, World!");
     }
 }
